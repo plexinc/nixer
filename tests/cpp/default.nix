563c6e3f4317860c999c4078669aa2b2696e621e
Sameer Rahmani <lxsameer@gnu.org>
2024-09-26 13:14:29 +0100

    Fix the cpp plain test to work with macos too

--- tests/cpp/default.nix
+++ tests/cpp/default.nix
@@ -19,16 +19,20 @@
 
           cmakeFlags = [ ];
           ninjaFlags = [ "-v" ];
-
-          checkPhase = ''
-            ${pkgs.pax-utils}/bin/lddtree main
-            ldd main|grep 'libc++' && echo "Success: Linked against libc++" || (echo "Error: Executable isn't link against libc++" && exit 1)
-            ldd main|grep 'libgcc' && echo "Error: Linked against libgcc???" && exit 1 || echo "Success: No libgcc"
-          '' + lib.strings.optionalString isLinux ''
-            ldd main|grep musl && echo "Success: Linked against Musl" || (echo "Error: Executable isn't link against Musl" && exit 1)
-          '' + lib.strings.optionalString true ''
-            ./main
-          '';
+          checkPhase =
+            if isLinux
+            then ''
+              ${pkgs.pax-utils}/bin/lddtree main
+              ldd main|grep 'libc++' && echo "Success: Linked against libc++" || (echo "Error: Executable isn't link against libc++" && exit 1)
+              ldd main|grep 'libgcc' && echo "Error: Linked against libgcc???" && exit 1 || echo "Success: No libgcc"
+              ldd main|grep musl && echo "Success: Linked against Musl" || (echo "Error: Executable isn't link against Musl" && exit 1)
+              ./main
+            ''
+            else ''
+              otool -L main|grep 'libc++' && echo "Success: Linked against libc++" || (echo "Error: Executable isn't link against libc++" && exit 1)
+              otool -L main|grep 'libgcc' && echo "Error: Linked against libgcc???" && exit 1 || echo "Success: No libgcc"
+              ./main
+            '';
         };
       };
     };
